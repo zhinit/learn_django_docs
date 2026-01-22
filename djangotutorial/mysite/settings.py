6d502e6646463d3772a5b787498a745a6c9f70f7
zachar-AI <zachskihill@gmail.com>
2026-01-21 22:10:19 -0500

    created Question and Choice models. got admin up and running

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/New_York"
 
 USE_I18N = True
 
